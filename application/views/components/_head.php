cfd8c38e01f27650ff942dbc1bbb261f3becb982
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-10-18 19:06:59 +0530

    Reset JS Optimize

--- application/views/components/_head.php
+++ application/views/components/_head.php
@@ -11,4 +11,4 @@
 <meta name="title" content="<?= APP_NAME ?>">
 <meta name="description" content="<?= APP_NAME ?>">
 
-<script src="<?= base_url("third-party/") ?>jquery3.7.1/jquery.min.js" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
