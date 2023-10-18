9fac19dd20f8083712e4ed677b794ee73b1d524d
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-10-18 19:14:59 +0530

    Optimize JS

--- application/views/components/_head.php
+++ application/views/components/_head.php
@@ -11,4 +11,4 @@
 <meta name="title" content="<?= APP_NAME ?>">
 <meta name="description" content="<?= APP_NAME ?>">
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
